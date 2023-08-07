f3d775c4cec628f1dcdd74ace69281db9e8584cc
galaraz <jvictor.si.2013@gmail.com>
2023-08-07 17:06:37 -0400

    cconfiguraçao firebase

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -11,31 +11,35 @@ interface MenuItemProps {
 export default function MenuItem(props: MenuItemProps) {
     function renderizarLink() {
         return (
-            <a className={`
-                    flex flex-col justify-center items-center
-                    h-20 w-20
-                    dark:text-gray-200
-                    ${props.className}
-                `}>
-                {props.icone}
-                <span className={`text-xs font-light`}>
-                    {props.texto}
-                </span>
-            </a>
-        )
-    }
-    return (
-        <li onClick={props.onClick} className={`
+          <div className={`
+            flex flex-col justify-center items-center
+            h-20 w-20
+            dark:text-gray-200
+            ${props.className}
+          `}>
+            {props.icone}
+            <span className={`text-xs font-light`}>
+              {props.texto}
+            </span>
+          </div>
+        );
+      }
+      
+      return (
+        <li
+          onClick={props.onClick}
+          className={`
             hover:bg-gray-100 dark:hover:bg-gray-800
             cursor-pointer
-        `}>
-            {props.url ? (
-                <Link href={props.url}>
-                    {renderizarLink()}
-                </Link>
-            ) : (
-                renderizarLink()
-            )}
+          `}
+        >
+          {props.url ? (
+            <Link href={props.url}>
+              {renderizarLink()}
+            </Link>
+          ) : (
+            renderizarLink()
+          )}
         </li>
-    )
-}
+      )
+    }
